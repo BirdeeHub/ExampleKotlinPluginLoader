0125f99f82f59fcbd88b8eb9e7dce07d53dac6a9
BirdeeHub <robindbor@gmail.com>
2023-08-04 11:20:22 -0700

    move the -all jar and the plugin jar into the same directory and run the loader to test

--- exampleplugin/bin/main/exampleplugin/MyPluginImplementation.kt
+++ exampleplugin/bin/main/exampleplugin/MyPluginImplementation.kt
@@ -4,6 +4,7 @@ import examplepluginloader.api.MyAPI
 import examplepluginloader.api.MyPlugin 
 class MyPluginImplementation : MyPlugin{
     override fun launchPlugin(api: MyAPI){
+        println(api.test())
     }
     override fun test(): String {
         return "123"
