e14c1c2fc433a39f0b91918454a92f97b44a12bb
BirdeeHub <robindbor@gmail.com>
2023-08-10 17:23:06 -0700

    changed some stuff about displaying the output

--- examplepluginloader/bin/main/examplepluginloader/program/MyProgram.kt
+++ examplepluginloader/bin/main/examplepluginloader/program/MyProgram.kt
@@ -6,9 +6,9 @@ class MyProgram(api: MyAPI, pluginPaths: Array<String>){
     init{
         println("Testing...")
         for (pluginPath in pluginPaths) {
-            for(plugID in PluginLoader.callPlugLoader(api, pluginPath)){
-                println(pluginPath)
-                var plugin: MyPlugin? = PluginLoader.getPlugin(plugID) // 1..
+            println(pluginPath)
+            for(plugID in PluginLoader.callPlugLoader(api, pluginPath)){// 1..
+                var plugin: MyPlugin? = PluginLoader.getPlugin(plugID)
                 println(plugin?.test())// 2..
                 println(plugID)
             }
