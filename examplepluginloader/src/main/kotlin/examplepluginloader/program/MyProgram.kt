5030fdb13c9db904b4ec05f12fdfbe0a07208f79
BirdeeHub <robindbor@gmail.com>
2023-08-10 17:01:56 -0700

    changed some stuff about displaying the output

--- examplepluginloader/src/main/kotlin/examplepluginloader/program/MyProgram.kt
+++ examplepluginloader/src/main/kotlin/examplepluginloader/program/MyProgram.kt
@@ -1,17 +1,16 @@
 package examplepluginloader.program
 import examplepluginloader.api.MyAPI
 import examplepluginloader.api.MyPlugin
-import java.util.UUID
 import examplepluginloader.PluggerXP.PluginLoader 
 class MyProgram(api: MyAPI, pluginPaths: Array<String>){
     init{
         println("Testing...")
-        val plugIDs: MutableList<UUID> = mutableListOf()
-        for (pluginPath in pluginPaths) plugIDs.addAll(PluginLoader.callPlugLoader(api, pluginPath))
-        for(plugID in plugIDs){
-            var plugin: MyPlugin? = PluginLoader.getPlugin(plugID)
-            println(plugin?.test())
-            println(plugID)
+        for (pluginPath in pluginPaths) {
+            for(plugID in PluginLoader.callPlugLoader(api, pluginPath)){
+                var plugin: MyPlugin? = PluginLoader.getPlugin(plugID)
+                println(plugin?.test())
+                println(plugID)
+            }
         }
         println("Goodbye!")
     }
