8252031c2d5d8142010639e7ad32ed8cba1e1492
BirdeeHub <robindbor@gmail.com>
2023-08-11 00:32:33 -0700

    README

--- exampleplugin/src/main/kotlin/exampleplugin/MyPluginImplementation.kt
+++ exampleplugin/src/main/kotlin/exampleplugin/MyPluginImplementation.kt
@@ -4,7 +4,8 @@ import examplepluginloader.api.MyAPI
 import examplepluginloader.api.MyPlugin 
 class MyPluginImplementation : MyPlugin{
     override fun launchPlugin(api: MyAPI){
-        println(api.test())
+        //normally you call your plugin's opening class from here and pass it the api instance.
+        println(api.test()) //<-- but this is a test so we just show that making calls to api works.
     }
     override fun test(): String {
         return "2..."
