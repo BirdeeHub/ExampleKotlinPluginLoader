f7224773db59a7156b35d57cfe8122ad36cce6ac
BirdeeHub <robindbor@gmail.com>
2023-08-10 20:22:44 -0700

    comments

--- examplepluginloader/src/main/kotlin/examplepluginloader/api/MyAPI.kt
+++ examplepluginloader/src/main/kotlin/examplepluginloader/api/MyAPI.kt
@@ -1,4 +1,5 @@
 package examplepluginloader.api
 public interface MyAPI {
     fun test() : String
+    //this would normally have a lot more things in it.
 }
--- examplepluginloader/src/main/kotlin/examplepluginloader/api/MyPlugin.kt
+++ examplepluginloader/src/main/kotlin/examplepluginloader/api/MyPlugin.kt
@@ -3,4 +3,5 @@ import examplepluginloader.api.MyAPI
 public interface MyPlugin {
     fun launchPlugin(api: MyAPI)
     fun test(): String
+    //this one probably wouldnt have anything else. It probably wouldn't even have test()
 }
