de831f324f315cf3bfeeda0ad66ebca285a606ff
BirdeeHub <robindbor@gmail.com>
2023-08-12 02:22:58 -0700

    Whoops

--- examplepluginloader/bin/main/examplepluginloader/main.kt
+++ examplepluginloader/bin/main/examplepluginloader/main.kt
@@ -1,4 +1,4 @@
 package examplepluginloader
 import examplepluginloader.api.PluggerXP.MyAPIobj
 import examplepluginloader.program.MyProgram
-fun main(args: Array<String>) = MyProgram(MyAPIobj(), args)
+fun main(args: Array<String>) { MyProgram(MyAPIobj(), args) }
