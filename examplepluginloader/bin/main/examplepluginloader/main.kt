3f92c7c0cb37bb149f0ab5091585ae9e120bc485
BirdeeHub <robindbor@gmail.com>
2023-08-11 18:43:02 -0700

    Thread-Safety

--- examplepluginloader/bin/main/examplepluginloader/main.kt
+++ examplepluginloader/bin/main/examplepluginloader/main.kt
@@ -1,6 +1,5 @@
 package examplepluginloader
 import examplepluginloader.api.MyPlugin
-import examplepluginloader.api.MyAPI
 import examplepluginloader.api.PluggerXP.MyAPIobj
 import examplepluginloader.program.MyProgram
 fun main(args: Array<String>) {
