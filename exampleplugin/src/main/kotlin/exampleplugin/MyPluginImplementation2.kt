95c2259ac429db1003ae4a9b26576c0524b80f95
BirdeeHub <robindbor@gmail.com>
2023-08-24 03:06:55 -0700

    Made one of the example plugins have an open thread to demonstrate that it really just might be because of swing that minsweeper's loader wont GC

--- exampleplugin/src/main/kotlin/exampleplugin/MyPluginImplementation2.kt
+++ exampleplugin/src/main/kotlin/exampleplugin/MyPluginImplementation2.kt
@@ -1,10 +1,30 @@
 package exampleplugin
 import examplepluginloader.api.MyAPI
-import examplepluginloader.api.MyPlugin 
+import examplepluginloader.api.MyPlugin
+import examplepluginloader.api.plugin.PluginUnloadHandler 
 class MyPluginImplementation2 : MyPlugin{
     override fun launchPlugin(api: MyAPI){
         //normally you call your plugin's opening class from here and pass it the api instance.
         //but this is a test so we just show that making calls to api works.
         println(api.plugin().pluginLocation().toString()) 
+        var looptest: Boolean = true
+        val thread = Thread {
+            try {
+                while(looptest){
+                    Thread.sleep(1000)
+                    print(1)
+                }
+                println("thread finished!")
+            } catch (e: InterruptedException) {
+                println("Thread was interrupted!")
+            }
+        }
+        api.plugin().registerShutdownSequence(object: PluginUnloadHandler{
+            override fun pluginUnloaded() {
+                looptest = false
+                thread.interrupt()
+            }
+        })
+        thread.start()
     }
 }
