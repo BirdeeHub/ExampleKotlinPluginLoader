8283c6a01683924813021550152b0a9221eba71c
BirdeeHub <robindbor@gmail.com>
2023-08-27 09:49:54 -0700

    JByteCodeURLINFO reworked to allow later danger scanning

--- examplepluginloader/bin/main/examplepluginloader/Plugger/JByteCodeURLINFO.kt
+++ examplepluginloader/bin/main/examplepluginloader/Plugger/JByteCodeURLINFO.kt
@@ -1,21 +1,31 @@
 package examplepluginloader.Plugger
 
+import java.net.HttpURLConnection
+import java.io.File
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.util.jar.JarInputStream
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Type
 import java.net.URL
 import java.util.UUID
 //This gets class info from a url and then after that just contains a list of the following interface and 3 functions
 class JByteCodeURLINFO(public val yourURL: URL){
-    val isSupported: Boolean
-    val classInfoAtURL: List<URLclassInfo>?
-    val rescInJar: List<URL>?
-    init{ 
-        val info = URLInfoGetter.getInfo(yourURL) 
-        isSupported = info.first
-        classInfoAtURL = info.second
-        rescInJar = info.third
+    val protocolSupported: Boolean
+    val classInfoAtURL: List<URLclassInfo>
+    private val rescInJar: Map<String,ByteArray>
+    private val urlBytes: ByteArray?
+    private val classBytes: Map<String,ByteArray>
+    private val urlDangerInfos = mutableListOf<CInfo>()
+    private interface CInfo {
+        val urURL: URL
+        val entryName: String
     }
     class URLclassInfo(
-        val urURL: URL,
+        override val urURL: URL,
+        override val entryName: String,
         val version: Int,
         val access: Int,
         val name: String?,
@@ -23,25 +33,146 @@ class JByteCodeURLINFO(public val yourURL: URL){
         val xtnds: String?,
         val imps: List<String>?,
         var optUUID: UUID?
-    ){
+    ): CInfo{
         fun isImpOf(internalName: String): Boolean = imps?.contains(internalName) ?: false
     }
-    //utility
     companion object {
         fun getExtClassName(internalName: String): String = 
             Type.getObjectType(internalName).getClassName()
         fun getInternalCName(obj: Class<*>): String = 
             Type.getInternalName(obj)
-        fun consolidateClassLists(infos: List<JByteCodeURLINFO>): List<URLclassInfo> { 
-            val infoList = mutableListOf<URLclassInfo>()
-            infos.forEach { if(it.classInfoAtURL!=null)infoList.addAll(it.classInfoAtURL) }
-            return infoList
+    }
+
+    init{ 
+        urlBytes = getBytesFromURL(yourURL)
+        if(urlBytes!=null){
+            if(yourURL.toString().endsWith(".jar")){
+                protocolSupported = true
+                val temp = bytesFromfromJar(urlBytes)
+                classBytes = temp.first ?: mapOf()
+                rescInJar = temp.second ?: mapOf()
+                val urlCInfos = mutableListOf<URLclassInfo>()
+                classBytes.forEach{urlCInfos.addAll(getCINFO(yourURL, it.key, it.value))}
+                classInfoAtURL = urlCInfos
+
+            }else if(yourURL.toString().endsWith(".class")){
+                protocolSupported = true
+                classBytes = mapOf(Pair(yourURL.file, urlBytes))
+                rescInJar = mapOf()
+                classInfoAtURL = getCINFO(yourURL, yourURL.file, urlBytes)
+                
+            } else {
+                protocolSupported = false
+                classBytes = mapOf()
+                rescInJar = mapOf()
+                classInfoAtURL = listOf()
+            }
+        } else {
+            protocolSupported = false
+            classBytes = mapOf()
+            rescInJar = mapOf()
+            classInfoAtURL = listOf()
         }
     }
-    fun getClassInfoByExtName(name: String): URLclassInfo? {
-        val tempList = classInfoAtURL?.filter { it.name == name.replace('.','/') }
-        if(tempList==null || tempList.isEmpty() ) return null
-        else if(tempList.size>1) throw Exception("Multiple classes found at URL with same fully qualified name")
-        else return tempList[0]
+    private fun getBytesFromURL(yourURL: URL): ByteArray?{
+        val bytesOfStuff: ByteArray?
+        if(yourURL.protocol == "file"){
+            bytesOfStuff = File(yourURL.toURI()).inputStream().readAllBytes()
+        }else if(yourURL.protocol == "http" || yourURL.protocol == "https"){
+            bytesOfStuff = getBytesFromHTTP(yourURL)
+        }else bytesOfStuff = null
+        return bytesOfStuff
+    }
+    //add other protocols here if desired
+    private fun getBytesFromHTTP(yourURL: URL): ByteArray? = try {
+        val urlBytes: ByteArray?
+        val urlConnection = yourURL.openConnection() as HttpURLConnection
+        urlConnection.requestMethod = "GET"
+        if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+            val inputStream = urlConnection.inputStream
+            urlBytes = inputStream.readBytes()
+            inputStream.close()
+            urlConnection.disconnect()
+        } else urlBytes = null
+        urlBytes
+    } catch (e: Exception) { e.printStackTrace(); null }
+    //This just calls defineClassFromBytes on jar entries
+    private fun bytesFromfromJar(jarBytes: ByteArray): Pair<Map<String,ByteArray>?, Map<String,ByteArray>?> {
+        val jarClassMap = mutableMapOf<String, ByteArray>()
+        val jarresourceMap = mutableMapOf<String,ByteArray>()
+        JarInputStream(ByteArrayInputStream(jarBytes)).use { jis ->
+            var entry = jis.getNextJarEntry()
+            while (entry!=null) {
+                if (!entry.isDirectory && entry.name.endsWith(".class")) {
+                    try{ jarClassMap[entry.name]=jis.readAllBytes()
+                    } catch (e: Exception){ e.printStackTrace() }
+                } else if(!entry.isDirectory)jarresourceMap[entry.name] = jis.readAllBytes()
+                jis.closeEntry()
+                entry = jis.getNextJarEntry()
+            }
+        }
+        if(jarClassMap.isEmpty() && jarresourceMap.isEmpty()) 
+            return Pair(null,null)
+        else if(jarClassMap.isEmpty() && !jarresourceMap.isEmpty()) 
+            return Pair(null,jarresourceMap)
+        else if(!jarClassMap.isEmpty() && jarresourceMap.isEmpty()) 
+            return Pair(jarClassMap,null)
+        else return Pair(jarClassMap,jarresourceMap)
+    }
+
+    //take byte arrays of .class files, Uses "org.ow2.asm:asm:9.5" to get info
+    private fun getCINFO(yourURL: URL, entryName: String, classBytes: ByteArray): List<URLclassInfo> {
+        val classInfo = mutableListOf<URLclassInfo>()
+        val classReader = ClassReader(classBytes)
+        classReader.accept(object : ClassVisitor(Opcodes.ASM9) {
+            override fun visit(version: Int, access: Int, name: String?, signature: String?, superName: String?, interfaces: Array<String>?) {
+                classInfo.add(URLclassInfo(yourURL, entryName, version, access, name, signature, superName, interfaces?.toList(), null))
+            }
+        }, ClassReader.SKIP_CODE or ClassReader.SKIP_DEBUG or ClassReader.SKIP_FRAMES)
+        return classInfo
+    }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------DANGER ZONE-------------DANGER ZONE--------------DANGER ZONE--------------DANGER ZONE--------DANGER ZONE--------DANGER ZONE--------DANGER ZONE----
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+//DO DANGERCHECKING HERE
+
+    private class ClassDangerCheckInfo(
+        override val urURL: URL,
+        override val entryName: String,
+        //TODO: Add all of the fields from ClassVisitors useful for finding danger
+    ): CInfo {}
+    private class RescDangerCheckInfo(
+        override val urURL: URL,
+        override val entryName: String,
+    ): CInfo {}
+    fun dangerCheck(): String{
+        val reconData = dangerScan()
+        //TODO, check the class and danger infos for danger
+        //TODO, process the results of that check, and scan info from getRescDangerInfo
+            //and turn them into nice report
+        return "dangerCheck() not yet implemented"
+    }
+    private fun dangerScan(): List<CInfo>{
+        classBytes.forEach { (k,v) -> urlDangerInfos.addAll(getClassDangerInfo(yourURL, k, v)) }
+        rescInJar.forEach { (k,v) -> urlDangerInfos.addAll(getRescDangerInfo(yourURL, k, v)) }
+        val totalCInfos = mutableListOf<CInfo>()
+        totalCInfos.addAll(classInfoAtURL)
+        totalCInfos.addAll(urlDangerInfos)
+        return totalCInfos
+    }
+    private fun getClassDangerInfo(yourURL: URL, entryName: String, classBytes: ByteArray): List<ClassDangerCheckInfo> {
+        val classInfo = mutableListOf<ClassDangerCheckInfo>()
+        /*val classReader = ClassReader(classBytes)
+        classReader.accept(object : ClassVisitor(Opcodes.ASM9) {
+            //TODO: visit all the other things and add the object(s) to the list so we can dangerCheck() later!
+            //Add them all to 1 object for ease of use later
+        }, ClassReader.EXPAND_FRAMES)*/
+        return classInfo
+    }
+    private fun getRescDangerInfo(yourURL: URL, entryName: String, RescBytes: ByteArray): List<RescDangerCheckInfo> {
+        val rescInfo = mutableListOf<RescDangerCheckInfo>()
+        //TODO scan the resource
+        return rescInfo
     }
 }
--- examplepluginloader/src/main/kotlin/examplepluginloader/Plugger/PluginManager.kt
+++ examplepluginloader/src/main/kotlin/examplepluginloader/Plugger/PluginManager.kt
@@ -20,7 +20,7 @@ object PluginManager {
     private val plugIDList = mutableListOf<UUID>() //<-- initialize our lists of stuff for loading and closing
     private val pluginObjectMap = mutableMapOf<UUID,MyPlugin>() //<-- this one has the loaded instances
     private val pluginCLMap: WeakHashMap<UUID,PluginLoader> = WeakHashMap<UUID,PluginLoader>() //<-- we will close these to unload plugins
-    private val classInfoByURLs = mutableMapOf<URL,JByteCodeURLINFO>() //<-- I made a reflections with ASM that works over web, which shouldnt hold any references
+    private val classInfoByURLs = mutableMapOf<URL,List<JByteCodeURLINFO.URLclassInfo>>() //<-- I made a reflections with ASM that works over web, which shouldnt hold any references
     private val pluginAPIobjs = mutableMapOf<UUID,MyAPI>() //<-- these have the references of each api object i pass to a plugin
     private val shutdownRegistrations = mutableListOf<UnloadPlugistration>() //<-- These have a reference to a plugin defined shutdown handler
 
@@ -37,30 +37,40 @@ object PluginManager {
     fun getPlugin(plugID: UUID): MyPlugin? = pluginObjectMap[plugID]
     fun getPluginUUID(plugin: MyPlugin): UUID? = pluginObjectMap.entries.find { it.value == plugin }?.key
     fun getPluginAPIobj(plugID: UUID): MyAPI? = pluginAPIobjs[plugID]
-    fun getCNamesAtURL(plugURL: URL): List<String>? =
-        classInfoByURLs[plugURL]?.classInfoAtURL?.filter { it.optUUID!=null }?.mapNotNull { it.name }
-    fun getPluginClassName(plugID: UUID): String? = try{ 
-        val name = JByteCodeURLINFO.consolidateClassLists(classInfoByURLs.mapNotNull {it.value})
-            .filter { it.optUUID == plugID }
-            .mapNotNull { it.name }
-        if(name.size != 1) null
-        else name.get(0)
-    }catch(e: Exception){null}
+    fun getPluginCNamesAtURL(plugURL: URL): List<String>? =
+        classInfoByURLs[plugURL]?.filter { it.optUUID!=null }
+            ?.mapNotNull { it.name }?.map { JByteCodeURLINFO.getExtClassName(it) }
+    fun getPluginClassName(plugID: UUID): String? { 
+        val name = mutableListOf<String>()
+        classInfoByURLs.forEach {
+            name.addAll(it.value.filter { it.optUUID == plugID }.mapNotNull {it.name})
+        }
+        return if(name.size != 1) null else JByteCodeURLINFO.getExtClassName(name.get(0))
+    }
     fun getPluginLocation(plugID: UUID): URL? = try{ 
         //only ever 1 url per uuid. 2 uuids for 1 url is possible but not relevant here, 
         //get(0) will throw error if UUID not found because list will be empty
-        classInfoByURLs.filter { it.value.classInfoAtURL
-            ?.any { it.optUUID == plugID } ?: false 
+        classInfoByURLs.filter { 
+            it.value.any { it.optUUID == plugID }
         }.map { it.key }.get(0) 
     }catch(e: Exception){null}
 
-    //This next one is for when you changed the name of the class that implements MyPlugin without restarting the program
-    // we have it keep the cache on remove because there may be more than 1 plugin per url, and also
-    //if we hang onto it, on reloads we can avoid doing the first of 2 downloads where we check for class names to run
-    fun clearInfoCacheForURL(pluginURL: URL) = classInfoByURLs.remove(pluginURL)
-
-    //public unload and load functions (Synchronized)
+    //Synchronized Public Functions
     private val lock = Any() // Shared lock object
+
+    //If you wish to dangerCheck URL, get JByteCodeURLINFO and run the danger check
+    //then put class info into here so you dont have to download it again
+    @Synchronized
+    fun addInfoCacheForURL(pluginClassInfo: List<JByteCodeURLINFO.URLclassInfo>){
+        val info = pluginClassInfo.filter{ 
+            it.isImpOf(JByteCodeURLINFO.getInternalCName(MyPlugin::class.java)) 
+        }
+        if(!info.isEmpty()) classInfoByURLs[info.get(0).urURL] = info
+    }
+    @Synchronized
+    fun clearInfoCacheForURL(pluginURL: URL) = classInfoByURLs.remove(pluginURL)
+    
+    //Unload Functions
     @Synchronized
     fun unloadPlugin(plugID: UUID){ //close and remove EVERYWHERE
         shutdownRegistrations.forEach {
@@ -118,11 +128,11 @@ object PluginManager {
         //no, this doesnt entirely work if you only GC 1 time...
         System.gc()
         try{
-            Thread.sleep(500)
+            Thread.sleep(250)
         }catch(e: InterruptedException){}
         System.gc()
         try{
-            Thread.sleep(750)
+            Thread.sleep(500)
         }catch(e: InterruptedException){}
     }
     @Synchronized
@@ -143,13 +153,15 @@ object PluginManager {
         //no, this doesnt entirely work if you only GC 1 time...
         System.gc()
         try{
-            Thread.sleep(500)
+            Thread.sleep(250)
         }catch(e: InterruptedException){}
         System.gc()
         try{
-            Thread.sleep(750)
+            Thread.sleep(500)
         }catch(e: InterruptedException){}
     }
+
+    //Public Load Functions
     @Synchronized
     fun loadPluginFile(pluginPathStrings: List<String>, targetPluginFullClassNames: List<String> = listOf()): List<UUID> {
         val pluginURIs = mutableListOf<URI>()
@@ -199,6 +211,8 @@ object PluginManager {
         val pluginsToRemove = mutableListOf<UUID>()
         for(pluginURI in pluginURIs){ //for each, call loadPluginsFromOneURI(pluginURI: URI, targetClassNames: List<String>): MutableList<UUID>
             val plugIDs = loadPluginsFromOneURI(pluginURI, targetPluginFullClassNames)//<-- loads the stuff, then we run launchPlugin(api) next
+            System.gc()
+            Thread.sleep(250)
             pluginUUIDs.addAll(plugIDs) //<-- add new IDs to list
             for (plugID in plugIDs) { //<-- our list from loadPluginsFromOneURI
                 try {
@@ -226,20 +240,18 @@ object PluginManager {
         val plugIDs = mutableListOf<UUID>()
         try{ // Step 1: getJarURLs(pluginPath: URI): List<URL>
             getJarURLs(pluginURI).forEach { plugURL ->
-                val pluginNames = mutableListOf<String>()
-                try{ // Step 2: get Class info at each url with JByteCodeURLINFO
-                            //WITHOUT LOADING THEM
-                    if(plugURL.protocol=="file")clearInfoCacheForURL(plugURL)
-                    if(classInfoByURLs[plugURL] == null)
-                        classInfoByURLs[plugURL]=JByteCodeURLINFO(plugURL)
-                    //Step 3: filter out instances of our plugin and add to names list
-                    classInfoByURLs[plugURL]?.classInfoAtURL?.filter{ 
+                try{ // Step 2: get Plugin info at each url with JByteCodeURLINFO
+                    //eventually, we can optionally pre-populate JByteCodeURLINFO.classInfoAtURL 
+                    //with a separate function so we can optionally virus scan before we load
+                    if(classInfoByURLs[plugURL] == null){
+                        classInfoByURLs[plugURL]=JByteCodeURLINFO(plugURL).classInfoAtURL.filter{ 
                             it.isImpOf(JByteCodeURLINFO.getInternalCName(MyPlugin::class.java)) 
-                        }?.mapNotNull {it.name}?.forEach { name -> 
-                            pluginNames.add(JByteCodeURLINFO.getExtClassName(name))
                         }
+                    }
+                    //Step 3: map to name and add to the list to pass to loadPluginClasses
+                    val pluginNames = classInfoByURLs[plugURL]?.mapNotNull {it.name}
                     // Step 4: create plugin instances and populate the globals at the top of file
-                    plugIDs.addAll(loadPluginClasses(plugURL, pluginNames, targetCNames))
+                    if(pluginNames!=null)plugIDs.addAll(loadPluginClasses(plugURL, pluginNames, targetCNames))
                 }catch(e: Exception){e.printStackTrace()}
             }
         }catch(e: Exception){e.printStackTrace()}
@@ -284,14 +296,14 @@ object PluginManager {
     }
     private fun loadPluginClass(plugURL: URL, pluginName: String, targetCNames: List<String> = listOf()): UUID? {
         //first, check our targets list
-        if(targetCNames.isEmpty()||(targetCNames.any { target -> ((pluginName == target)) })){
+        if(targetCNames.isEmpty()||(targetCNames.any { target -> (JByteCodeURLINFO.getExtClassName(pluginName) == target) })){
             //then get instance, UUID, then update global list/maps. Return new UUID so user knows which were 
             var loader: PluginLoader? = null
             try{
                 val pluginUUID = UUID.randomUUID() //<-- Use a UUID to keep track of them.
                 loader = PluginLoader(plugURL, pluginUUID, PluginManager::class.java.classLoader.parent) //PluginManager is running under MyProgramLoader. Get parent, which is MySystemLoader
-                val pluginInstance = loader.loadClass(pluginName).getConstructor().newInstance() as MyPlugin //<-- this will throw if theres a name collision in VM
-                classInfoByURLs[plugURL]?.getClassInfoByExtName(pluginName)?.optUUID = pluginUUID //<-- getClassInfoByName throws if name collision at url
+                val pluginInstance = loader.loadClass(JByteCodeURLINFO.getExtClassName(pluginName)).getConstructor().newInstance() as MyPlugin //<-- this will throw if theres a name collision in jar
+                classInfoByURLs[plugURL]?.find { it.name==pluginName }?.optUUID = pluginUUID
                 pluginAPIobjs[pluginUUID] = MyAPIobj(pluginUUID) //<-- create a new api obj for each one so that we can manage them individually
                 pluginObjectMap[pluginUUID] = pluginInstance
                 pluginCLMap[pluginUUID] = loader //<-- we keep track of PluginLoaders so we can close them later
